daa6a51e084f3c69349bce24a953f63c6350ef56
Kristina Stefanac <kristina.stefanac123@email.com>
2026-07-22 23:15:17 +0200

    Dodana Pagination komponenta

---
+++ src/components/Pagination.vue
@@ -0,0 +1,20 @@
+<script setup>
+const props = defineProps({
+  page: { type: Number, required: true },
+  pageCount: { type: Number, required: true }
+})
+defineEmits(['change'])
+</script>
+
+<template>
+  <div v-if="pageCount > 1" class="pagination">
+    <button class="secondary" :disabled="page === 1" @click="$emit('change', page - 1)">‹ Prev</button>
+    <button
+      v-for="n in pageCount"
+      :key="n"
+      :class="{ active: n === page }"
+      @click="$emit('change', n)"
+    >{{ n }}</button>
+    <button class="secondary" :disabled="page === pageCount" @click="$emit('change', page + 1)">Next ›</button>
+  </div>
+</template>
