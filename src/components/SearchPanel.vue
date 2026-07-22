c7b4b3f4a71be5331163cbeabab39ee89b25cd29
Kristina Stefanac <kristina.stefanac123@email.com>
2026-07-22 23:26:03 +0200

    Dodana SearchPanel komponenta

---
+++ src/components/SearchPanel.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({ modelValue: String })
+defineEmits(['update:modelValue'])
+</script>
+
+<template>
+  <div class="row" style="margin-bottom: 16px;">
+    <input
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+      placeholder="Search bikes by name…"
+    />
+  </div>
+</template>
