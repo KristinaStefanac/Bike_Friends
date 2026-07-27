3a3b056404a18a2cada0334f3eb1de085782b6c5
Kristina Stefanac <kristina.stefanac123@email.com>
2026-07-27 23:41:20 +0200

    Dodana ruta za stranicu s detaljima bicikla

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Login from '../views/Login.vue'
-import Home from '../views/Home.vue'
+import { useAuthStore } from '../stores/auth.js'
+
+const routes = [
+  { path: '/', name: 'home', component: () => import('../views/Home.vue') },
+  { path: '/login', name: 'login', component: () => import('../views/Login.vue') },
+  { path: '/bikes/:id', name: 'bike', component: () => import('../views/BikeDetail.vue') },
+  { path: '/my-bikes', name: 'myBikes', component: () => import('../views/MyBikes.vue'), meta: { auth: true } },
+  { path: '/my-bikes/new', name: 'bikeNew', component: () => import('../views/BikeForm.vue'), meta: { auth: true } },
+  { path: '/my-bikes/:id/edit', name: 'bikeEdit', component: () => import('../views/BikeForm.vue'), meta: { auth: true } },
+  { path: '/messages', name: 'messages', component: () => import('../views/Messages.vue'), meta: { auth: true } },
+  { path: '/messages/:threadId', name: 'thread', component: () => import('../views/Messages.vue'), meta: { auth: true } }
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
+  history: createWebHistory(),
+  routes
+})
 
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-
-    {
-      path: '/login',
-      name: 'login',
-      component: Login
-    }
-  ]
+router.beforeEach(async (to) => {
+  if (!to.meta.auth) return true
+  const auth = useAuthStore()
+  await auth.ready
+  if (!auth.user) {
+    return { name: 'login', query: { redirect: to.fullPath } }
+  }
+  return true
 })
 
 export default router
